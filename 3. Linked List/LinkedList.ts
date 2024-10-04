8a2d572908fe9324e38189f0de535f0493cf7878
Divs-Git <divyansh.sri258@gmail.com>
2024-10-05 01:51:11 +0530

    added

--- 3. Linked List/LinkedList.ts
+++ 3. Linked List/LinkedList.ts
@@ -61,14 +61,74 @@ function removeAtTail(head: Head | null) {
   return head;
 }
 
+function addAtIndex(head: Head | null, index: number, data: number) {
+  if (head == null) return null;
+  if (index === 0) return addAtHead(head, data);
+
+  let newNode = createNewNode(data);
+  let prev: Head | null = null;
+  let temp: Head | null = head;
+  let cnt = 0;
+  while (temp) {
+    if (cnt === index) {
+      if (prev) {
+        prev.next = newNode;
+      }
+      newNode.next = temp;
+      return head;
+    }
+    prev = temp;
+    temp = temp.next;
+    cnt++;
+  }
+
+  if (cnt === index) {
+    if (prev) {
+      prev.next = newNode;
+    }
+  }
+
+  return head;
+}
+
+function removeAtIndex(head: Head | null, index: number) {
+  if (head === null) return null;
+  if (index === 0) return removeAtHead(head);
+
+  let temp: Head | null = head;
+  let cnt = 0;
+  let prev: Head | null = null;
+
+  while (temp) {
+    if (cnt === index) {
+      if (prev) {
+        prev.next = temp.next;
+        temp.next = null;
+        return head;
+      }
+    }
+    cnt++;
+    prev = temp;
+    temp = temp.next;
+  }
+
+  if (cnt <= index) {
+    console.error("Index out of bounds");
+    return head;
+  }
+
+  return head;
+}
 let head = null;
 
 head = addAtHead(head, 30);
 head = addAtHead(head, 20);
 head = addAtHead(head, 10);
 head = addAtTail(head, 40);
-head = removeAtHead(head);
-head = removeAtTail(head);
+// head = removeAtHead(head);
+// head = removeAtTail(head);
+head = addAtIndex(head, 4, 99);
+head = removeAtIndex(head, 6);
 printLL(head);
 
 // console.log(head);
